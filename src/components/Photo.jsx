990cedfda2e6114a82833ca91756b73b9bceef8b
MiricleDev <rohan13k@outlook.com>
2025-03-03 01:30:54 +0000

    Added new project

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -7,7 +7,7 @@ const Photo = () => {
 
     const [count, setCount] = useState(0)
 
-    const photos = ['1 (1).jpg', '1 (2).jpg', '1 (3).jpg', '1 (4).jpg', '1 (5).jpg', '1 (6).jpg', 'pfp.PNG']
+    const photos = [`${process.env.PUBLIC_URL}/1 (1).jpg`, `${process.env.PUBLIC_URL}/1 (2).jpg`, `${process.env.PUBLIC_URL}/1 (3).jpg`, `${process.env.PUBLIC_URL}/1 (4).jpg`, `${process.env.PUBLIC_URL}/1 (5).jpg`, `${process.env.PUBLIC_URL}/1 (6).jpg`, `${process.env.PUBLIC_URL}/pfp.PNG`]
 
     const decrement = () => {
         setCount(prevCount => (prevCount-1 >= 0 ? prevCount - 1 : photos.length - 1))
--- src/projects.js
+++ src/projects.js
@@ -3,7 +3,7 @@ export const projects = [
 {
     id: 2,
     title: 'Chimba Spanish',
-    photo: '/chimba_logo.png',
+    photo: `${process.env.PUBLIC_URL}/chimba_logo.png`,
     description: "",
     stack: [],
     github: "https://github.com/miricledev/chimba-spanish",
@@ -14,7 +14,7 @@ export const projects = [
 {
     id: 3,
     title: 'Bitewise',
-    photo: '/bitewise_logo.png',
+    photo: `${process.env.PUBLIC_URL}/bitewise_logo.png`,
     description: "",
     stack: [],
     github: 'https://github.com/SP-Sauce/RoyalHackathon_W_Team',
@@ -25,13 +25,24 @@ export const projects = [
 {
     id: 4,
     title: 'Calendar App',
-    photo: '/calendar_app.png',
+    photo: `${process.env.PUBLIC_URL}/calendar_app.png`,
     description: "",
     stack: [],
     github: "https://github.com/miricledev/Miricles-Calendar-App",
     type: 'Frontend',
     date_completed: '26/08/24',
     complete: true
+},
+{
+    id: 5,
+    title: "Portfolio V1",
+    photo: `${process.env.PUBLIC_URL}/portfolio_v1.png`,
+    description: "",
+    stack: [],
+    github: "https://github.com/miricledev/software-portfolio",
+    type: "Frontend",
+    date_completed: "In progress",
+    complete: false
 }
 
 
